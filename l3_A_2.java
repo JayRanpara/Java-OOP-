94c13576b19b27c4dcdd6f2e554d45a607e13bd4
JayRanpara <jayranpara9282@gmail.com>
2025-01-16 20:36:59 +0530

    change file

--- l3_A_2.java
+++ l3_A_2.java
@@ -1,18 +1,18 @@
-// import java.util.Scanner;
+import java.util.Scanner;
 
-// public class l3_A_2{
-// 	public static void main(String args[]) {
-// 		Scanner sc =new Scanner(System.in);
-// 		System.out.println("Enter Number = ");
-// 		int n = sc.nextInt();
-// 		if(n%2 == 0) {
-// 			System.out.println("even number");
-// 		}
-// 		else {
-// 			System.out.println("odd Number");
-// 		}
-// 	}
-// }
+public class l3_A_2{
+	public static void main(String args[]) {
+		Scanner sc =new Scanner(System.in);
+		System.out.println("Enter Number = ");
+		int n = sc.nextInt();
+		if(n%2 == 0) {
+			System.out.println("even number");
+		}
+		else {
+			System.out.println("odd Number");
+		}
+	}
+}
 
 import java.util.Scanner;
 
