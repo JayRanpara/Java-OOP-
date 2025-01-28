bd77bda90d8c5de56099141721bc30e92a7de033
JayRanpara <jayranpara9282@gmail.com>
2025-01-28 13:21:14 +0530

    first commit

--- OOPs lab/l5_B_6.java
+++ OOPs lab/l5_B_6.java
@@ -1,6 +1,9 @@
+import java.util.Scanner;
+
 public class l5_B_6{
 	public static void main(String args[]) {
-		int n = 3;
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
 		for(int i=0; i<n; i++) {
 			for(int j = 0; j<n-i; j++) {
 				System.out.print(" ");
